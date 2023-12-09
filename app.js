a3736b170ea8acff193e59d8feecfc2003f8ece4
Shubham Bandgar <106601480+Shubham28062003@users.noreply.github.com>
2023-12-10 00:16:41 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,23 @@
+// const heading = React.createElement("h1",{},"Hello React !!");
+//     const root = ReactDOM.createRoot(document.getElementById("root"));
+//     root.render(heading);
+
+
+const heading = React.createElement("h1", {
+    id : "heading"
+},
+"Hello React This is heading 1")
+
+const container = React.createElement("div",{
+    id : "container",
+    className : "class-container"
+},["This is Div",heading])
+
+
+
+
+
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(container);
